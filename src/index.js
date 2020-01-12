5d0df59f7ea89192307585b8ff0cfc5c173de8c0
André Lins <andrelucas01@hotmail.com>
2020-01-12 12:00:20 -0300

    Error in cors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes');
-const swaggerDoc = require('./swaggerDoc');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes");
+const swaggerDoc = require("./swaggerDoc");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
 
 routes(app);
 swaggerDoc(app);
-
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
-app.use(morgan('dev'));
 
 const port = process.env.PORT || 8080;
 
